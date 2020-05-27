64e6141ce57e94b187f0a0c170d27b5e50236aef
Shadab420 <shadab_anwar@outlook.com>
2020-05-27 12:38:44 +0600

    added contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,19 @@ import Layout from "../components/Layout"
 const contact = () => {
   return (
     <Layout>
-      <h2>Contact page</h2>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Get in touch</h3>
+          <form method="post" data-netlify="true">
+            <div className="form-group">
+              <input type="text" placeholder="name" className="form-control"/>
+              <input type="email" placeholder="email" className="form-control"/>
+              <textarea name="message" rows="5" placeholder="message" className="form-control"></textarea>
+            </div>
+            <button type="submit" className="btn submit-btn">Submit</button>
+          </form>
+        </article>
+      </section>
     </Layout>
     
   )
